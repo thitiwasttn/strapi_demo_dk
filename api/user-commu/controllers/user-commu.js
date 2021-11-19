25ae92b33aa4f3fa61efa7d85189f51427c49e8a
GA007007\Admin <thitiwas.n@g-able.com>
2021-11-19 13:58:16 +0700

    add custom api

--- api/user-commu/controllers/user-commu.js
+++ api/user-commu/controllers/user-commu.js
@@ -1,8 +1,16 @@
-'use strict';
+const {sanitizeEntity} = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async byUser(ctx) {
+    const userId = ctx.state.user.id;
+    console.log(await strapi.query('user-commu').find({}));
+    const newVar = await strapi.query('user-commu').find({users_permissions_user: userId});
+    console.log(newVar);
+    return newVar;
+  },
+};
