d13f6fed687a6d4bc6cc895fea416869f18840b6
GA007007\Admin <thitiwas.n@g-able.com>
2021-11-20 22:32:41 +0700

    up

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: 'mysql',
         host: env('DATABASE_HOST', '61.19.242.56'),
         port: env.int('DATABASE_PORT', 3304),
-        database: env('DATABASE_NAME', 'strapi_dk_personal'),
+        database: env('DATABASE_NAME', 'strapi_dk'),
         username: env('DATABASE_USERNAME', 'root'),
         password: env('DATABASE_PASSWORD', 'password'),
       },
