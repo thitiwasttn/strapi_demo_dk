1d8d2915e75242377aa82de7a816bc3979094c5e
GA007007\Admin <thitiwas.n@g-able.com>
2021-11-22 11:18:09 +0700

    up

--- config/database.js
+++ config/database.js
@@ -5,9 +5,9 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', '172.17.0.1'),
+        host: env('DATABASE_HOST', '61.19.242.56'),
         port: env.int('DATABASE_PORT', 3304),
-        database: env('DATABASE_NAME', 'strapi_dk'),
+        database: env('DATABASE_NAME', 'strapi_dk_personal'),
         username: env('DATABASE_USERNAME', 'root'),
         password: env('DATABASE_PASSWORD', 'password'),
       },
